73e7709cae586e20eeed30534f323c64c9f77e57
Anilkumar5654 <565413anil@gmail.com>
2025-12-08 18:02:38 +0530

    Create create.tsx

---
+++ app/stories/create.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import StoryUploader from '@/components/stories/StoryUploader';
+
+export default function CreateStoryScreen() {
+  // Sara logic ab StoryUploader ke andar hai
+  return <StoryUploader />;
+}
